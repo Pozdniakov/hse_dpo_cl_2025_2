05245a9a5806ad456d17cf2c88436c0fa1ac846f
Pozdniakov <bucherr@yandex.ru>
2025-05-20 14:52:46 +0200

    add first day

---
+++ 01_rintro.R
@@ -0,0 +1,104 @@
+2 + 3
+2 - 1
+7 * 3
+15 / 2
+15 %/% 2
+15 %% 2
+6 ^ 2
+6 ** 2 #возведение в степень в стиле Python тоже работает,
+#но не принято
+(2 + 2) * 2
+
+16 ^ 0.5
+sqrt(16)
+log(8)
+?log
+
+log(x = 8, base = 2)
+log(8, 2)
+?`+`
+`+`(2, 3)
+1/0
+1/ (5 ^ 2)
+sin(pi)
+options(scipen = 999)
+typeof(sin(pi))
+options(scipen = 6)
+2 ^ 100
+options(scipen = 999)
+2 ^ 100
+sin(pi)
+1/6
+
+a = 2 #у нас тут так не принято!
+b <- 3
+#5 -> d
+a ^ b - b / a  + a
+log(b, a)
+e <- a ^ b
+rm(e)
+
+a == b
+
+a != b
+factorial(5)
+
+5!=120
+`!=`(5, 120)
+?`!=`
+
+a > b
+a < b
+a >= b
+a >= a
+a <= b
+a <= a
+
+is.integer(a)
+is.integer(5L)
+a <- 2L
+is.integer(a)
+pi
+letters
+LETTERS
+month.name
+month.abb
+exp(1)
+typeof(3+2i)
+
+s <- "hi, everyone"
+s <- 'hi, everyone'
+s
+typeof("2")
+#"r" + "love" #не работает
+paste("I", "love", "R")
+?paste
+paste("I", "love", "R", sep = "_")
+paste("I", "love", "R", sep = "_<3_****---")
+paste("I", "love", "R", sep = "")
+paste0("I", "love", "R")
+
+typeof(a > b)
+comparison <- a > b
+f1 <- FALSE
+t1 <- TRUE
+#print(t1)
+t1
+!f1
+!t1
+
+t1 & t1 #TRUE
+t1 & f1 #FALSE
+f1 & t1 #FALSE
+f1 & f1 #FALSE
+
+t1 | t1 #TRUE
+t1 | f1 #TRUE
+f1 | t1 #TRUE
+f1 | f1 #FALSE
+
+#xor(t1, t1)
+
+c(4, 8, 15, 16, 23, 42)
+c("hey", "hey", "ho")
+c(FALSE, TRUE, FALSE, TRUE)
